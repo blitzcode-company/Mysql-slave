53b6e8a67eaeceab1ca2545b864684d211130c9a
MateeSR <92068513+MateeSR@users.noreply.github.com>
2026-07-05 04:24:35 -0300

    Enable SSL for MySQL replication

--- init-slave.sql
+++ init-slave.sql
@@ -3,6 +3,8 @@ CHANGE MASTER TO
     MASTER_PORT=3306,
     MASTER_USER='replication',
     MASTER_PASSWORD='Repli2024.',
-    MASTER_SSL=0;
-
+    MASTER_SSL=1,
+    MASTER_SSL_CA='/etc/mysql/ssl/ca.pem',
+    MASTER_SSL_CERT='/etc/mysql/ssl/client-cert.pem',
+    MASTER_SSL_KEY='/etc/mysql/ssl/client-key.pem';
 START SLAVE;
